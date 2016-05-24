1b56e0eb791b5a276bce08a9fdcdc1246f444a5d
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2016-05-24 21:30:22 +0800

    commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # gethools
 tools for geth dapp development
+
+
+# usage
+
+run `npm install -g` in this folder to update the terminal commands defined in
+`package.json` "bin"
+
+then run `gest [cmd]` as specified in `package.json` "bin". This requires the above; one could also
+add a command like `gesht` to `~/.bash_profile` to avoid compiling each time, which I prefer (that
+requires the code path).
+
+
+[Useful](https://developer.atlassian.com/blog/2015/11/scripting-with-node/)
--- index.js
+++ index.js
@@ -1,3 +1,40 @@
-module.export = {
-  toInterface: require('./lib/interface.js')
-};
+#!/usr/bin/env node
+
+
+var functions = require('./lib');
+
+// get arguments
+var program = require('commander');
+
+program
+  .version('0.0.1')
+  .usage('[options] <function...> <args>')
+  .arguments('<functionName> [args...]')
+  .action(function(functionName, args) {
+
+    if (!functions[functionName]) {
+      console.log('Invalid function name');
+      return;
+    }
+
+    functions[functionName](args);
+  })
+  .parse(process.argv);
+
+// program
+//   .arguments('<functionName>')
+//   // .option('-u, --username <username>', 'The user to authenticate as')
+//   // .option('-p, --password <password>', 'The user\'s password')
+//   .action(function(functionName) {
+//     //  console.log('user: %s pass: %s file: %s',
+//     //      program.username, program.password, file);
+//     console.log(functionName);
+//     if (!functions[functionName]) {
+//       console.log('Invalid function name');
+//       return;
+//     }
+//   })
+//   .parse(process.argv);
+
+
+// console.log('Hello, world!');
