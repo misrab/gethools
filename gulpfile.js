cbf22f707deb8b4eb52cd18b468920c37114708c
Misrab M. Faizullah-Khan <faizullah.misrab@gmail.com>
2016-06-15 23:34:20 +0800

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -29,5 +29,5 @@ gulp.task('geth', shell.task([
   'mkdir ~/.misrab_testnet',
   'mkdir ~/.misrab_testnet/keystore',
   'mv ~/UTC--2016-06-05T07-33-51.586286128Z--939cdcccc1b84523e77a8964c5d8de7cdc2e8a41 ~/.misrab_testnet/keystore/UTC--2016-06-05T07-33-51.586286128Z--939cdcccc1b84523e77a8964c5d8de7cdc2e8a41',
-  'geth --mine --genesis ~/geth/test_genesis.json --port "30304" --networkid 9347  --datadir ~/.misrab_testnet --nodiscover --maxpeers 0  --rpcport "8546" --rpc --rpcapi "eth,web3,personal" --rpccorsdomain "http://localhost:3000" 2>> test.log',
+  'geth --mine --genesis ~/geth/test_genesis.json --port "30304" --networkid 9347  --datadir ~/.misrab_testnet --nodiscover --maxpeers 0  --rpcport "8546" --rpc --rpcapi "eth,web3,personal" --rpccorsdomain "http://localhost:3000" console' //2>> test.log',
 ]));
